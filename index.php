595f1d4c0af7c8895e6e6fd28c396004becaec50
Adrian <Adrianrastegari@yahoo.se>
2018-10-22 09:48:31 +0200

    koden för databasen är klar och själva databasen

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title> </title>
+</head>
+<body>
+   
+   
+    <form action="submit.php" method="get">
+
+        Name: <input type="Name" name="Name" placeholder="Type name"><br>
+
+        Email: <input type="Email" name="Email" placeholder="Type email"><br>
+        
+        Age: <input type="age" name="age" placeholder="Age"><br>
+
+        Male: <input type="radio" name="gender" value="Male"><br>
+
+        Female: <input type="radio" name="gender" value="Female"><br>
+        
+        telnr: <input type="tel" name="Telephone">
+        <br>
+        
+        Lösenord: <input type="password" name="password">
+
+        <input type="submit" value="Skicka">
+
+    </form>
+    
+</body>
+</html>
